9a00c5437a18e279c1a26d8253723b7de3ae5769
M'aiq <m.janik1031@gmail.com>
2021-04-17 19:33:32 -0500

    Add proj files

---
+++ InfiniteCasings/src/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using Deli.Setup;
+
+namespace InfiniteCasings
+{
+	public class InfiniteCasings : DeliBehaviour
+	{
+		private readonly Hooks _hooks;
+
+		public InfiniteCasings()
+		{
+			_hooks = new Hooks();
+		}
+
+		private void OnDestroy()
+		{
+			_hooks?.Dispose();
+		}
+	}
+}
